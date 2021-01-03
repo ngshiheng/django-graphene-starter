4e1b134e9b1a68410be00661c7484766eb950628
Jerry Ng <ngshiheng@gmail.com>
2021-01-03 12:17:57 +0800

    Add delete and update mutation for Reporter

--- django_graphene_starter/starter/mutations.py
+++ django_graphene_starter/starter/mutations.py
@@ -1,4 +1,4 @@
-from graphene import ID, ClientIDMutation, DateTime, Enum, Field, List, String
+from graphene import ID, ClientIDMutation, Field, String
 from graphql import GraphQLError
 from graphql_relay import from_global_id
 
@@ -15,7 +15,7 @@ class CreateReporter(ClientIDMutation):
         email = String(required=True)
 
     @classmethod
-    def mutate_and_get_payload(cls, _, info, **input):
+    def mutate_and_get_payload(cls, root, info, **input):
 
         first_name = input['first_name']
         last_name = input['last_name']
@@ -33,3 +33,45 @@ class CreateReporter(ClientIDMutation):
             raise GraphQLError('Reporter already exist!')
 
         return CreateReporter(reporter=reporter)
+
+
+class UpdateReporter(ClientIDMutation):
+    reporter = Field(ReporterNode)
+
+    class Input:
+        id = ID(required=True, description='ID of the Reporter to be updated.')
+        first_name = String()
+        last_name = String()
+        email = String()
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+
+        _, id = from_global_id(input['id'])
+
+        reporter = Reporter.objects.get(id=id)
+
+        for field, value in input.items():
+            if field != 'id':
+                setattr(reporter, field, value)
+
+        reporter.full_clean()
+        reporter.save()
+
+        return UpdateReporter(reporter=reporter)
+
+
+class DeleteReporter(ClientIDMutation):
+    reporter = Field(ReporterNode)
+
+    class Input:
+        id = ID(required=True, description='ID of the Reporter to be deleted.')
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+
+        _, id = from_global_id(input['id'])
+
+        reporter = Reporter.objects.get(id=id).delete()
+
+        return DeleteReporter(reporter=reporter)
--- django_graphene_starter/starter/schema.py
+++ django_graphene_starter/starter/schema.py
@@ -2,12 +2,14 @@ from graphene import ObjectType
 from graphene.relay import Node
 from graphene_django.filter import DjangoFilterConnectionField
 
-from .mutations import CreateReporter
+from .mutations import CreateReporter, DeleteReporter, UpdateReporter
 from .types import ReporterNode
 
 
 class Mutation(ObjectType):
     create_reporter = CreateReporter.Field(description='Create a single Reporter.')
+    update_reporter = UpdateReporter.Field(description='Update a single Reporter.')
+    delete_reporter = DeleteReporter.Field(description='Delete a single Reporter.')
 
 
 class Query(ObjectType):
