add8410bb60be2e974245b661e99c3fc51b29d81
Jerry Ng <ngshiheng@gmail.com>
2021-01-03 13:02:05 +0800

    Add tests for Publication object

---
+++ django_graphene_starter/starter/tests/test_publication.py
@@ -0,0 +1,135 @@
+import json
+
+from django_graphene_starter.schema import schema
+from graphene_django.utils.testing import GraphQLTestCase
+from graphql_relay import to_global_id
+from mixer.backend.django import mixer
+
+from ..models import Publication
+
+PUBLICATIONS_QUERY = '''
+query publications {
+  publications {
+    totalCount
+    edges {
+      node {
+        id
+      }
+    }
+  }
+}
+'''
+
+
+CREATE_PUBLICATION_MUTATION = '''
+mutation createPublication($input: CreatePublicationInput!) {
+  createPublication(input: $input) {
+    publication {
+      id
+      title
+    }
+  }
+}
+
+'''
+
+UPDATE_PUBLICATION_MUTATION = '''
+mutation updatePublication($input: UpdatePublicationInput!) {
+  updatePublication(input: $input) {
+    publication {
+      id
+      title
+    }
+  }
+}
+
+'''
+
+DELETE_PUBLICATION_MUTATION = '''
+mutation deletePublication($input: DeletePublicationInput!) {
+  deletePublication(input: $input) {
+    publication {
+      id
+      title
+    }
+  }
+}
+
+'''
+
+
+class PublicationTestCase(GraphQLTestCase):
+    GRAPHQL_SCHEMA = schema
+    GRAPHQL_URL = '/graphql'
+
+    def setUp(self):
+        self.publication1 = mixer.blend(Publication)
+        self.publication2 = mixer.blend(Publication)
+
+    def test_publications_query(self):
+        response = self.query(
+          PUBLICATIONS_QUERY,
+          op_name='publications',
+        )
+        self.assertResponseNoErrors(response)
+        content = json.loads(response.content)
+
+        self.assertEqual(len(content['data']['publications']['edges']), 2)
+        self.assertEqual(content['data']['publications']['totalCount'], 2)
+
+    def test_create_publication_mutation(self):
+
+        response = self.query(
+          CREATE_PUBLICATION_MUTATION,
+          op_name='createPublication',
+          variables={
+            'input': {
+              'title': 'Reverse-engineered intangible application',
+            }
+          }
+        )
+        self.assertResponseNoErrors(response)
+        content = json.loads(response.content)
+
+        self.assertIsNotNone(content['data']['createPublication']['publication']['id'])
+        self.assertEqual(content['data']['createPublication']['publication']['title'], 'Reverse-engineered intangible application')
+
+    def test_update_publication_mutation(self):
+
+        id = to_global_id('PublicationNode', self.publication1.id)
+
+        response = self.query(
+          UPDATE_PUBLICATION_MUTATION,
+          op_name='updatePublication',
+          variables={
+            'input': {
+              'id': id,
+              'title': 'Function-based homogeneous synergy',
+            }
+          }
+        )
+
+        self.assertResponseNoErrors(response)
+        content = json.loads(response.content)
+        self.assertEqual(content['data']['updatePublication']['publication']['id'], id)
+        self.assertEqual(content['data']['updatePublication']['publication']['title'], 'Function-based homogeneous synergy')
+
+    def test_delete_publication_mutation(self):
+        another_publication_id = to_global_id('PublicationNode', self.publication2.id)
+        number_of_publications = Publication.objects.count()
+
+        response = self.query(
+          DELETE_PUBLICATION_MUTATION,
+          op_name='deletePublication',
+          variables={
+            'input': {
+              'id': another_publication_id,
+            }
+          }
+        )
+
+        self.assertResponseNoErrors(response)
+        content = json.loads(response.content)
+
+        self.assertEqual(number_of_publications - 1, Publication.objects.count())
+        self.assertEqual(content['data']['deletePublication']['publication']['id'], 'UHVibGljYXRpb25Ob2RlOk5vbmU=')  # PublicationNode:None
