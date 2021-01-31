e77c36ba92c543416c744748687d326fbfecb2da
Jerry Ng <ngshiheng@gmail.com>
2021-01-31 15:00:13 +0800

    Add whitenoise

--- django_graphene_starter/django_graphene_starter/settings.py
+++ django_graphene_starter/django_graphene_starter/settings.py
@@ -108,7 +108,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 GRAPHENE = {
     'RELAY_CONNECTION_MAX_LIMIT': 5000,
