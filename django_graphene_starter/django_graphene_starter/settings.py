79e8a9fed1c7a6f32cc831388af3c25202382ddf
Jerry Ng <ngshiheng@gmail.com>
2021-01-31 16:19:13 +0800

    Use postgres if DATABASE_URL is provided else sqlite

--- django_graphene_starter/django_graphene_starter/settings.py
+++ django_graphene_starter/django_graphene_starter/settings.py
@@ -1,8 +1,6 @@
 import os
 from pathlib import Path
 
-import dj_database_url
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -67,17 +65,16 @@ WSGI_APPLICATION = 'django_graphene_starter.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG:
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
+
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }
-
-else:
-    DATABASES = {
-        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
 
 # Password validation
