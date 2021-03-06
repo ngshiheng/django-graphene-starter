959ac2cb79a4b01891860aa34a146ea8a4bd456c
Jerry Ng <ngshiheng@gmail.com>
2021-03-06 15:47:54 +0800

    Add basic rate limit

--- django_graphene_starter/django_graphene_starter/urls.py
+++ django_graphene_starter/django_graphene_starter/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
-from graphene_django.views import GraphQLView
+
+from django_graphene_starter.views import RateLimitedGraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path('graphql', csrf_exempt(RateLimitedGraphQLView.as_view(graphiql=True))),
 ]
