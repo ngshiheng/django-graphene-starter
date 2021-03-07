288d503a2bb478ce6b00f33faf2b82421e1249b3
Jerry Ng <ngshiheng@gmail.com>
2021-03-07 17:52:09 +0800

    Update urls.py path

--- django_graphene_starter/django_graphene_starter/urls.py
+++ django_graphene_starter/django_graphene_starter/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
@@ -8,7 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql', csrf_exempt(RateLimitedGraphQLView.as_view(graphiql=True))),
     path('hello', HelloView.as_view()),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 handler404 = 'django_graphene_starter.views.custom_page_not_found_view'
