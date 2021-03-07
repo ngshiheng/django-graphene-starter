a25c0bf115b826811feaee77b025cc7b525094fd
Jerry Ng <ngshiheng@gmail.com>
2021-03-07 14:59:53 +0800

    Add a hello world view

--- django_graphene_starter/django_graphene_starter/urls.py
+++ django_graphene_starter/django_graphene_starter/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-from django_graphene_starter.views import RateLimitedGraphQLView
+from django_graphene_starter.views import HelloView, RateLimitedGraphQLView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('graphql', csrf_exempt(RateLimitedGraphQLView.as_view(graphiql=True))),
+    path('hello', HelloView.as_view()),
 ]
--- django_graphene_starter/django_graphene_starter/views.py
+++ django_graphene_starter/django_graphene_starter/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.http.request import HttpRequest
 from django.http.response import JsonResponse
 from django.utils.decorators import method_decorator
+from django.views.generic import View
 from graphene_django.views import GraphQLView
 from graphql.execution.base import ExecutionResult
 from ratelimit.decorators import ratelimit
@@ -12,6 +13,17 @@ from ratelimit.decorators import ratelimit
 from django_graphene_starter.utils import get_client_ip
 
 logger = logging.getLogger(__name__)
+
+
+@method_decorator(ratelimit(key='user_or_ip', rate=settings.RATELIMIT_RATE, method='GET', block=True), name='get')
+class HelloView(View):
+    """
+    A simple hello world
+    """
+
+    def get(self, request: HttpRequest, *args, **kwargs) -> JsonResponse:
+        logger.info(f'Client with IP Address {get_client_ip(request)} is saying hi!')
+        return JsonResponse({'message': 'Hello, 世界!'})
 
 
 @method_decorator(ratelimit(key='user_or_ip', rate=settings.RATELIMIT_RATE, method=ratelimit.ALL, block=True), name='execute_graphql_request')
@@ -25,5 +37,9 @@ class RateLimitedGraphQLView(GraphQLView):
 
 
 def ratelimited_error(request: HttpRequest, exception: Exception) -> JsonResponse:
+    """
+    Returns rate limit error to the client if
+    """
+
     logger.warning(f'Client with IP Address {get_client_ip(request)} is making requests exceeding the rate limit of {settings.RATELIMIT_RATE}!')
     return JsonResponse({'error': 'You are making too many requests! Slow down and enjoy the moment you’re in.'}, status=429)
