c4b62fa24baf57f2749c074bb3ff7b3ac6412f45
Jerry Ng <ngshiheng@gmail.com>
2021-03-08 21:40:45 +0800

    Use dj-static to serve static files

--- django_graphene_starter/django_graphene_starter/settings.py
+++ django_graphene_starter/django_graphene_starter/settings.py
@@ -181,6 +181,5 @@ RATELIMIT_RATE = os.environ.get('RATELIMIT_RATE', '5/s')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'django_graphene_starter', 'static')
--- django_graphene_starter/django_graphene_starter/wsgi.py
+++ django_graphene_starter/django_graphene_starter/wsgi.py
@@ -1,7 +1,8 @@
 import os
 
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_graphene_starter.settings')
+application = Cling(get_wsgi_application())
 
-application = get_wsgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_graphene_starter.settings')
