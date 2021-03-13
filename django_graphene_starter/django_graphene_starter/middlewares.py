58503b18cd52aefdd71f8352e9dd36622e097239
Jerry Ng <ngshiheng@gmail.com>
2021-03-13 17:06:42 +0800

    Update logging configs

--- django_graphene_starter/django_graphene_starter/middlewares.py
+++ django_graphene_starter/django_graphene_starter/middlewares.py
@@ -1,9 +1,7 @@
 import starter.loaders as loaders
+from graphene import ResolveInfo
 from sentry_sdk import capture_exception
-from sentry_sdk.integrations.logging import ignore_logger
 from starter.types import ArticleNode, ReporterNode
-
-ignore_logger('graphql.execution.utils')
 
 
 class SentryMiddleware(object):
@@ -16,7 +14,17 @@ class SentryMiddleware(object):
         capture_exception(error)
         raise error
 
-    def resolve(self, next, root, info, **args):
+    def resolve(self, next, root, info: ResolveInfo, **args):
+        """
+        This will run on every single GraphQL field.
+
+        You can think of each field in a GraphQL query as a function or method of the previous type which returns the next type.
+        In fact, this is exactly how GraphQL works.
+        Each field on each type is backed by a function called the resolver which is provided by the GraphQL server developer.
+        When a field is executed, the corresponding resolver is called to produce the next value.
+
+        Reference: https://graphql.org/learn/execution/
+        """
         return next(root, info, **args).catch(self.on_error)
 
 
@@ -25,12 +33,11 @@ class Loaders:
         self.reporter_by_article_loader = loaders.generate_loader(ReporterNode, "id")()
         self.articles_by_reporter_loader = loaders.generate_loader_by_foreign_key(ArticleNode, 'reporter_id')()
 
-        self.articles_by_publication_loader = loaders.generate_loader_by_many_to_many_key(ArticleNode, 'publications')()  # TODO: Need fixes. This is not working as expected
+        self.articles_by_publication_loader = loaders.generate_loader_by_many_to_many_key(ArticleNode, 'publications')()  # FIXME: This is not working as expected
 
 
 class LoaderMiddleware:
-    def resolve(self, next, root, info, **args):
-
+    def resolve(self, next, root, info: ResolveInfo, **args):
         if not hasattr(info.context, 'loaders'):
             info.context.loaders = Loaders()
 
