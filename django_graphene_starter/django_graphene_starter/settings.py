d06b80fde7018a04ba0d25d87c8c23cbf6a05856
Jerry Ng <ngshiheng@gmail.com>
2021-03-13 12:02:53 +0800

    Add django extensions to ease debugging and admin.py

--- django_graphene_starter/django_graphene_starter/settings.py
+++ django_graphene_starter/django_graphene_starter/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'graphene_django',
     'starter',
 ]
