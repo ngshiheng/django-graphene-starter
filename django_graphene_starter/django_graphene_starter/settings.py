3de5a23a09ba33aa36d6894a050ee9a0448d3974
Jerry Ng <ngshiheng@gmail.com>
2021-07-25 17:13:56 +0800

    Update staticfiles path

--- django_graphene_starter/django_graphene_starter/settings.py
+++ django_graphene_starter/django_graphene_starter/settings.py
@@ -211,6 +211,12 @@ RATELIMIT_RATE = os.environ.get('RATELIMIT_RATE', '5/s')
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
